8477a7636de3952d889926d42fcf00f07b0f6f5b
Mohamadreza <mr.jabarzade2005@gmail.com>
2025-05-15 16:26:49 +0330

    psr4 auto load for factories bug fixed

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\comment>
  */
-class commentFactory extends Factory
+class CommentFactory extends Factory
 {
     /**
      * Define the model's default state.
